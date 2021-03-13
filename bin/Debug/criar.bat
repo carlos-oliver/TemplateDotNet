cb02c16bbf1a98f2278ee8ce31a70d225997ade3
carlos-oliver <carlos.st.oliveira@gmail.com>
2021-03-13 07:28:18 -0300

    first commit

---
+++ bin/Debug/criar.bat
@@ -0,0 +1,4 @@
+@echo off
+cls
+dotnet new -u AdatumCorporation.Utility.Templates
+dotnet new -i AdatumCorporation.Utility.Templates.1.0.0.nupkg
---
+++ templates/itau/Sample.Run/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Itau.FJ5.Test.E2E.Run
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
