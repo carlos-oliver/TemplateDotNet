eeed84626fed234f5a1a71ab9f26db29541d1c0d
carlos-oliver <carlos.st.oliveira@gmail.com>
2021-03-15 09:18:00 -0300

    Atualização de projeto

--- bin/Debug/criar.bat
+++ bin/Debug/criar.bat
@@ -1,4 +1,4 @@
 @echo off
 cls
 dotnet new -u AdatumCorporation.Utility.Templates
-dotnet new -i AdatumCorporation.Utility.Templates.1.0.0.nupkg
+::dotnet new -i AdatumCorporation.Utility.Templates.1.0.0.nupkg
